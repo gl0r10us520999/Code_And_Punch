8d6d366b63c42d52f038c92c4e72a2199fbd8841
Do Minh Huy <112995017+I3IN1202@users.noreply.github.com>
2023-05-27 20:22:42 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" >
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <form>
+        <input action="" method="POST">
+        username:</br> <input type="text" name="username" /> </br>
+        password:</br> <input type="password" name="password" /> </br>
+        <input type="submit" class="button" name="dangnhap" value="Đăng nhập" />
+
+    </form>
+</body>
+</html>
