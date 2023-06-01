26cefcee12502b2646da1b9ba436f0047be00312
Gl0r10us_520999 <124490947+gl0r10us520999@users.noreply.github.com>
2023-06-01 17:32:32 +0700

    login.php

--- login.php
+++ login.php
@@ -2,24 +2,18 @@
 require './functions.php';
 //declare use session
 session_start();
-
-//declare utf-8 
-header('Content-Type: text/html; charset=UTF-8');
+connectDB();
 
 //handle login
 
-if (isset($_POST['login'])) {
-
-    connectDB();
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //get input from user
     $username = $_POST['username'];
-    $password = hash('sha256', isset($_POST['password']) ? $_POST['password'] : "");
+    $password = $_POST['password'];
 
     //check input username and password
     if (!$username || !$password) {
-        $message_error = "Please enter the full login username and password!";
-        echo "<script type='text/javascript'>alert('$message_error');</script>";
+        echo "Invalid username or password!";
         exit;
     }
 
@@ -27,7 +21,7 @@ if (isset($_POST['login'])) {
     $regex2 = preg_match('/[\'"^£$%&*()}{@#~?><>,|=_+¬-]/', $password);
     if (!$regex1 && !$regex2) {
 
-        $query = "SELECT id,username,password,score FROM users WHERE username = ? and password = ?";
+        $query = "SELECT * FROM users WHERE username = ? and password = ?";
 
         // Prevent SQLi by using prepared statement
         $preparedStatement = $conn->prepare($query);
@@ -35,33 +29,33 @@ if (isset($_POST['login'])) {
         $preparedStatement->execute();
         $result = $preparedStatement->get_result();
 
-        if (mysqli_num_rows($result) <= 0) {
-            $message = "Incorrect username or password!";
-            echo "<script type='text/javascript'>alert('$message');</script>";
-        } else {
-            //Save login info
+        if ($result->num_rows > 0) {
+            // Get information form DB
             $row = $result->fetch_assoc();
-            $_SESSION['id'] = $row['id'];
-            $_SESSION['username'] = $row['username']; 
-            $_SESSION['id']=$row['id'];
-            $_SESSION['name']=$row['name'];
-            $_SESSION['email'] = $row['email'];
-            $_SESSION['phone_number'] = $row['phone_number'];
-             if ($row["type"] == "student") {
-                // Kiểm tra quyền của sinh viên và chuyển hướng tới trang student.php
-                header("Location: studentHome.php");
+            // save session's data
+            if ($password == $row['password']) {
+                $_SESSION["username"] = $username;
+                $_SESSION['id'] = $row['id'];
+                $_SESSION['name'] = $row['name'];
+                $_SESSION['role'] = $row['role'];
+                $_SESSION['email'] = $row['email'];
+                $_SESSION['phone_number'] = $row['phone_number'];
+                //Check role of user and redirect to home page
+                if ($row["role"] == "student") {
+                    header("Location: Lab2_4.php");
+                    exit;
+                } elseif ($row["role"] == "teacher") {
+                    header("Location: Add_Student.php");
+                    exit;
+                }
+            } else {
+                echo "Wrong password!";
                 exit;
-            } elseif ($row["type"] == "teacher") {
-                // Chuyển hướng tới trang teacher.php
-                header("Location: teacherHome.php");
-                exit;
-            }else {
-                echo"Wrong username or password";
             }
+        } else {
+            echo "Username does not exist!";
+            exit;
         }
-    } else {
-        http_response_code(400);
-        die('Error processing bad or malformed request');
     }
 }
 disconnectDB();
@@ -71,54 +65,85 @@ disconnectDB();
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login</title>
-        <link href="http://fonts.cdnfonts.com/css/audiowide" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-              integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-              crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <script src="https://cdn.tailwindcss.com"></script>
-        <link rel="stylesheet" href="./css/style.css">
-        <script>
-            tailwind.config = {
-                theme: {
-                    extend: {
-                        colors: {
-                            primary: '#02bacd',
-                        }
-                    }
-                }
-            }
-        </script>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+          integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
 
-    <body>
+        .h-custom-2 {
+            height: calc(100% - 6.5rem);
+        }
+        .bg-image-vertical {
+        position: relative;
+        overflow: hidden;
+        background-repeat: no-repeat;
+        background-position: right center;
+        background-size: auto 100%;
+        }
 
-        <div id="main" class="bg-black h-[100vh] w-[100vw]">
-            <div class="flex flex-col items-center py-10">
-                <h1 class="text-black hidden md:block md:text-[3rem]">Login</h1>
+        @media (min-width: 1025px) {
+        .h-custom-2 {
+        height: 100%;
+        }
+        }
+    </style>
+</head>
 
-                <form action="login" method="POST" class="pt-5">
-                    <div class="py-1">
-                        <span class="text-white">Username</span>
-                        <input type="text" name="username" id="username"
-                               class="block text-center bg-transparent text-white font-medium border border-white border-[2px] rounded-2xl px-3 py-2 mt-2 md:w-[350px]">
-                    </div>
-                    <div class="py-1">
-                        <span class="text-white">Password</span>
-                        <input type="password" name="password" id="password"
-                               class="block text-center bg-transparent text-white font-medium border border-white border-[2px] rounded-2xl px-3 py-2 mt-2 md:w-[350px]">
-                    </div>
-                    <div class="text-center pt-5">
-                        <input type="submit" name="login" value="Login"
-                               class="border border-[2px] border-white px-4 py-2 rounded-full text-white hover:text-black hover:bg-gray-400 cursor-pointer">
-                    </div>
-                </form>
+<body>
+<section class="vh-100">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6 text-black">
+
+                <div class="px-5 ms-xl-4">
+                    <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+                    <span class="h1 fw-bold mb-0">Login</span>
+                </div>
+
+                <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+
+                    <form style="width: 23rem;" action="" method="POST">
+                        <!-- Add CSRF token -->
+                        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
+
+                        <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
+
+                        <div class="form-outline mb-4">
+                            <input type="text" id="form2Example28" class="form-control form-control-lg" name="username" required />
+                            <label class="form-label" for="form2Example18">Username</label>
+                        </div>
+
+                        <div class="form-outline mb-4">
+                            <input type="password" id="form2Example28" class="form-control form-control-lg" name="password" required />
+                            <label class="form-label" for="form2Example28">Password</label>
+                        </div>
+
+                        <div class="pt-1 mb-4">
+                            <button class="btn btn-info btn-lg btn-block" type="submit">Login</button>
+                        </div>
+
+                        <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
+                        <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+                    </form>
+
+                </div>
+
+            </div>
+            <div class="col-sm-6 px-0 d-none d-sm-block">
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fvov2.vov.vn%2Fgiao-duc-dao-tao%2Fdai-hoc-fpt-bao-dong-do-toan-truong-vi-ca-nhiem-covid-19-24481.vov2&psig=AOvVaw0PMd1-bjGrDufEWbwaaBoE&ust=1685701879549000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCKj3oP_uof8CFQAAAAAdAAAAABAE"
+                     alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
             </div>
         </div>
-    </body>
+    </div>
+</section>
+</body>
 
 </html>
